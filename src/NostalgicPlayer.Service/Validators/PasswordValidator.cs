db958df69ef0707f1797101bb1b50436accb11d8
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-07-18 00:20:14 +0500

    Did UniTests for strong password

---
+++ src/NostalgicPlayer.Service/Validators/PasswordValidator.cs
@@ -0,0 +1,30 @@
+﻿namespace NostalgicPlayer.Service.Validators;
+
+public class PasswordValidator
+{
+    private static string Symbols { get; set; } = "~`! @#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
+
+    public static (bool IsValid, string Message) IsStrongPassword(string password)
+    {
+        if (password.Length < 8) return (IsValid: false, Message: "Password cannot be less than 8 characters!");
+
+        bool isUpperCaseExists = false;
+        bool isLowerCaseExists = false;
+        bool isDigitExists = false;
+        bool isCharacterExists = false;
+
+        foreach (var item in password)
+        {
+            if (char.IsUpper(item)) isUpperCaseExists = true;
+            if (char.IsLower(item)) isLowerCaseExists = true;
+            if (char.IsDigit(item)) isDigitExists = true;
+            if (Symbols.Contains(item)) isCharacterExists = true;
+        }
+        if (isDigitExists == false) return (IsValid: false, Message: "Password should contain at least a digit!");
+        if (isUpperCaseExists == false) return (IsValid: false, Message: "Password should contain at least an uppercase letter!");
+        if (isLowerCaseExists == false) return (IsValid: false, Message: "Password should contain at least an uppercase letter!");
+        if (isCharacterExists == false) return (IsValid: false, Message: "Password should contain at least a symbol!");
+
+        return (IsValid: true, "");
+    }
+}
---
+++ test/NostalgicPlayer.UnitTest/ValidatorTests/PasswordValidatorTest.cs
@@ -0,0 +1,40 @@
+﻿using NostalgicPlayer.Service.Validators;
+
+namespace NostalgicPlayer.UnitTest.ValidatorTests;
+
+public class PasswordValidatorTest
+{
+    [Theory]
+    [InlineData("AAaa55##")]
+    [InlineData("Aa55#!@$")]
+    [InlineData("kbHeb2%.")]
+    [InlineData("$AAAhe2%&")]
+    [InlineData("_abcdEFG8")]
+    [InlineData("aBCd884^")]
+    [InlineData("!23()Ads")]
+    [InlineData("123AAa#*")]
+    [InlineData("#vbA=32*")]
+    public void IsStrongPassword(string password)
+    {
+        var result = PasswordValidator.IsStrongPassword(password);
+        Assert.True(result.IsValid);
+    }
+
+    [Theory]
+    [InlineData("asAA12@")]
+    [InlineData("asAAdfg!")]
+    [InlineData("KJAAA12#")]
+    [InlineData("KJAAA12a")]
+    [InlineData("ababABAB")]
+    [InlineData("w4t4g12@")]
+    [InlineData("w4t4g1@ ")]
+    [InlineData("DSGSEGER")]
+    [InlineData("ergerrrr")]
+    [InlineData("@%#@%$@#")]
+    [InlineData("34534643")]
+    public void IsWeakPassword(string password)
+    {
+        var result = PasswordValidator.IsStrongPassword(password);
+        Assert.False(result.IsValid);
+    }
+}
