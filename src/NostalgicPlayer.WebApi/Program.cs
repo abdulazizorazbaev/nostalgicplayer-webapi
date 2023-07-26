a792e938e204a8c8eb2d4ef665324e42fa673fc9
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-07-27 00:17:37 +0500

    Bugs on program.cs has been fixed

--- src/NostalgicPlayer.WebApi/Program.cs
+++ src/NostalgicPlayer.WebApi/Program.cs
@@ -17,7 +17,7 @@ builder.ConfigureServiceLayer();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
